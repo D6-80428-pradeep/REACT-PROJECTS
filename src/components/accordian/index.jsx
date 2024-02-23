206c54fd963b33f19ef150ad94b295acbff81df7
D6-80428-pradeep <pradeepsinghsatna1998@gmail.com>
2024-02-23 18:23:31 +0530

    sigle selection accordian is done

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -2,24 +2,35 @@
 // multi selection both accordian im working 
 import data from "./data";
 
-
+import './style.css';
 import { useState } from "react"
 
 
-export default function Accordian(){
+export default function Accordian() {
 
-    const[selected,setSelected]=useState(null);
+    const [selected, setSelected] = useState(null);
+    function handleSingleSelection(getCurrentId) {
+        console.log(getCurrentId);
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+
+    }
+    console.log(selected);
     return <div className="wrapper">
         <div className="accordian">
             {
-                data && data.length >0 ? 
-                data.map(dataItem=><div className="item">
-                    <div className="title">
-                      <h3>{dataItem.question}</h3>
-                      <span>++</span>
-                    </div>
-                </div>)
-                :<div>No data found !!!</div>
+                data && data.length > 0 ?
+                    data.map(dataItem => <div className="item">
+                        <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
+                        </div>
+                        {
+                            selected === dataItem.id ?
+                                <div className="content">{dataItem.answer}</div>
+                                : null
+                        }
+                    </div>)
+                    : <div>No data found !!!</div>
             }
         </div></div>
 }
