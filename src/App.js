52b93cdf10f765e70671fd21a09379a746dc155f
D6-80428-pradeep <pradeepsinghsatna1998@gmail.com>
2024-02-23 16:53:23 +0530

    my first react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello this is my react project</h1>
+    </div>
+  );
+}
+
+export default App;
