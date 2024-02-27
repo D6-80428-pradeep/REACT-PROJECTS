30f23c82a2e9c3a797cf545ebd494965ff740b20
D6-80428-pradeep <pradeepsinghsatna1998@gmail.com>
2024-02-28 00:12:27 +0530

    star-rating project done

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-colour';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
 
       {/* Accordian  */}
-      {/* {
+      {
         <Accordian />
 
-      } */}
-       
+      }
+
       {/* Random colour generation */}
       {
         <RandomColor />
       }
+      <StarRating noOfStars={10} />
 
     </div>
   );
